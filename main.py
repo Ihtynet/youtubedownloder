9e7b5006ee50d0f79973469d0f192e362c404aa3
Владимир <orion76@list.ru>
2024-03-23 23:22:22 +0300

    213

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pytube import YouTube
-f = open("links.txt","r")
+f = open("links2.txt","r")
 mas = f.read().split("\n")
 for link in mas:
+    print(link)
     myStream = YouTube(link).streams.first()
     myStream.download()
-    
